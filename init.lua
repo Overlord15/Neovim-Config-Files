65aec59903bbb5e2591db04ec1f5d726c46924d8
Overlord15 <khamrai.eren.3@gmail.com>
2023-01-07 20:18:36 +0530

    Files added

---
+++ init.lua
@@ -0,0 +1,10 @@
+require('anupam.base')
+require('anupam.highlights')
+require('anupam.maps')
+require('anupam.plugins')
+
+local has = vim.fn.has
+local is_win = has "win32"
+
+require('anupam.windows')
+
